8ff4ef786f33dcc6e40d32d0b2790bfd9abb2405
jakehl <jakelangford@outlook.com>
2019-12-04 13:25:36 +0000

    day 02, part 2

--- 2/main.go
+++ 2/main.go
@@ -16,8 +16,8 @@ func getOperationValues(inputs []int) (val1, val2, position int) {
 	return
 }
 
-func main() {
-	var inputsTxt, err = utils.ReadTextFile("input.txt")
+func getInputsFromFile(filename string) []int {
+	var inputsTxt, err = utils.ReadTextFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,7 +30,13 @@ func main() {
 		}
 		inputs[i] = int(integer)
 	}
+	return inputs
+}
 
+func getOutputFromInputs(noun, verb int) int {
+	inputs := getInputsFromFile("input.txt")
+	inputs[1] = noun
+	inputs[2] = verb
 	for i := 0; i < len(inputs); i += 4 {
 		if inputs[i] == 1 {
 			val1, val2, position := getOperationValues(inputs[i+1 : i+4])
@@ -38,9 +44,23 @@ func main() {
 		} else if inputs[i] == 2 {
 			val1, val2, position := getOperationValues(inputs[i+1 : i+4])
 			inputs[position] = inputs[val1] * inputs[val2]
-		} else if inputs[i] == 9 {
+		} else if inputs[i] == 99 {
 			break
 		}
 	}
-	fmt.Print(inputs)
+	return inputs[0]
+}
+
+func main() {
+	var output, noun, verb = 0, 0, 0
+	for ; output != 19690720 && noun < 99; noun++ {
+		verb = 0
+		for ; output != 19690720 && verb < 99; verb++ {
+			output = getOutputFromInputs(noun, verb)
+
+			if output == 19690720 {
+				fmt.Printf("found: 100 * [noun: %v] + [verb: %v] = [%v]; output = %v\n", noun, verb, 100*noun+verb, output)
+			}
+		}
+	}
 }
